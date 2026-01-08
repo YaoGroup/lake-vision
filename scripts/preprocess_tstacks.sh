34d781063b3bca42eee8934d108674749f591d88
jharlanr <joshuahrines@gmail.com>
2026-01-08 09:56:05 -0800

    merge

--- scripts/preprocess_tstacks.sh
+++ scripts/preprocess_tstacks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=proc_tstacks
-#SBATCH --output=/oak/stanford/groups/cyaolai/JoshRines/repos/lake-vision/sherlock/logs/%x_%j.out
-#SBATCH --error=/oak/stanford/groups/cyaolai/JoshRines/repos/lake-vision/sherlock/logs/%x_%j.err
+#SBATCH --output=/oak/stanford/groups/cyaolai/JoshRines/sherlock/sherlock_lakevision/logs/%x_%j.out
+#SBATCH --error=/oak/stanford/groups/cyaolai/JoshRines/sherlock/sherlock_lakevision/logs/%x_%j.err
 #SBATCH --time=08:00:00
 #SBATCH -p serc
 #SBATCH --nodes=1
@@ -26,8 +26,8 @@ pip install --user xarray netcdf4
 REPO_DIR="/oak/stanford/groups/cyaolai/JoshRines/repos/lake-vision"
 TSTACK_DIR="/oak/stanford/groups/cyaolai/JoshRines/data/tstacks/CW2019_tstacks"
 AREA_FILE="/oak/stanford/groups/cyaolai/JoshRines/data/all_lakes_2019.nc"
-OUTPUT_DIR="/oak/stanford/groups/cyaolai/JoshRines/data/processed"
-MAX_LAKES=20  # Set to empty string for all lakes, or a number to limit
+OUTPUT_DIR="/oak/stanford/groups/cyaolai/JoshRines/data/tstacks/processed"
+MAX_LAKES=  # Set to empty string for all lakes, or a number to limit
 
 # Create output directory
 mkdir -p $OUTPUT_DIR
