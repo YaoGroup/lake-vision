d1118209c9a81211a5e01434350570aa269645d9
jrines <joshuahrines@gmail.com>
2026-01-15 11:51:04 -0800

    updated preprocess_tstacks.sh

--- scripts/preprocess_tstacks.sh
+++ scripts/preprocess_tstacks.sh
@@ -26,7 +26,7 @@ pip install --user xarray netcdf4
 REPO_DIR="/oak/stanford/groups/cyaolai/JoshRines/repos/lake-vision"
 TSTACK_DIR="/oak/stanford/groups/cyaolai/JoshRines/data/tstacks/CW2019_tstacks"
 AREA_FILE="/oak/stanford/groups/cyaolai/JoshRines/data/all_lakes_2019.nc"
-OUTPUT_DIR="/oak/stanford/groups/cyaolai/JoshRines/data/tstacks/processed"
+OUTPUT_DIR="/oak/stanford/groups/cyaolai/JoshRines/data/tstacks/CW2019_tstacks_processed"
 MAX_LAKES=  # Set to empty string for all lakes, or a number to limit
 
 # Spectral bands configuration
